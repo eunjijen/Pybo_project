f9a42a92fab023c051e5b62f3af9f3bb9b4fd331
lee <eunji.jennifer@gmail.com>
2023-02-27 14:37:12 +0900

    Answer 삭제,수정 완성

--- pybo/forms.py
+++ pybo/forms.py
@@ -8,10 +8,10 @@ class QuestionForm(forms.ModelForm):
 
 # 메타 데이터는 실제 데이터를 설명하는 데이터
 
-        # widget = {
-        #     'subject' : forms.TextInput(attrs={'class': 'form-control'}),
-        #     'content' : forms.Textarea(attrs={'class': 'form-control', 'rows':10}),
-        # }
+        widgets = {
+            'subject' : forms.TextInput(attrs={'placeholder': '제목을 입력하세요.'}),
+            'content' : forms.Textarea(attrs={'placeholder': '내용을 입력하세요.'}),
+        }
 
 class AnswerForm(forms.ModelForm):
     class Meta:
